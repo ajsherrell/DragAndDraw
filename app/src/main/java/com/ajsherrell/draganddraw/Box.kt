dd3deb4b743b4c28830ebfbd674ce19e1aa80820
ajsherrell <ajsherrell.ajacks@gmail.com>
2020-04-23 12:13:54 -0400

    add box class for a single box

---
+++ app/src/main/java/com/ajsherrell/draganddraw/Box.kt
@@ -0,0 +1,21 @@
+package com.ajsherrell.draganddraw
+
+import android.graphics.PointF
+
+class Box(val start: PointF) {
+
+    var end: PointF = start
+
+    val left: Float
+        get() = Math.min(start.x, end.y)
+
+    val right: Float
+        get() = Math.max(start.x, end.y)
+
+    val top: Float
+        get() = Math.min(start.x, end.y)
+
+    val bottom: Float
+        get() = Math.max(start.x, end.y)
+
+}
